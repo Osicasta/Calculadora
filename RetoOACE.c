530b8b24278b8d08597f4e20a7c19187d0099a3e
Oscar <oscar.castano@upb.edu.co>
2024-02-16 00:12:57 -0500

    Terminado

--- RetoOACE.c
+++ RetoOACE.c
@@ -1,132 +1,185 @@
 #include <stdio.h>
 #include <math.h>
 
-float suma(float num1, float num2) {
-    return num1 + num2;
-}
+float stack[8];
 
-float resta(float num1, float num2) {
-    return num1 - num2;
-}
-
-float multiplicacion(float num1, float num2) {
-    return num1 * num2;
-}
-
-float division(float num1, float num2) {
-    if (num2 != 0) {
-        return num1 / num2;
-    } else {
-        printf("Error: División por cero\n");
-        return 0;
+void mostrarPila(void)
+{
+    int i;
+    for (i = 7; i >= 0; i--)
+    {
+        printf("%d. "
+               "%f \n",
+               i + 1, stack[i]);
     }
 }
 
-float seno(float num) {
-    return sin(num);
+void ingresarNum(void)
+{
+    float num;
+    printf("Ingrese el número: ");
+    scanf("%f", &num);
+    int i;
+    for (i = 7; i >= 0; i--)
+    {
+        stack[i] = stack[i - 1];
+    }
+    stack[0] = num;
 }
 
-float coseno(float num) {
-    return cos(num);
+void mover(void)
+{
+    int i;
+    for (i = 1; i < 8; i++)
+    {
+        stack[i] = stack[i + 1];
+    }
 }
 
-float tangente(float num) {
-    return tan(num);
-}
+void selecopera(void)
+{
+    char oper;
+    float resultado;
+    printf("Ingrese la operación que desea realizar \n"
+           "+, -, *, /, r(raiz cuadrada), p(potencia), s(seno), c(coseno), t(tangente)\n");
+    scanf("%s", &oper);
+    switch (oper)
+    {
+    case '+':
+        resultado = stack[0] + stack[1];
+        stack[0] = resultado;
+        mover();
+        break;
 
-int main() {
-    int teclado, operacion;
-    float num1, num2, resultado;
+    case '-':
+        resultado = stack[1] + stack[0];
+        stack[0] = resultado;
+        mover();
+        break;
 
-    printf("Menú\n");
+    case '*':
+        resultado = stack[0] * stack[1];
+        stack[0] = resultado;
+        mover();
+        break;
 
-    do {
-        printf("1. Ingresar un número:\n");
-        printf("2. Seleccionar Operación:\n");
-        printf("3. Limpiar Ultimo\n");
-        printf("4. Limpiar todo\n");
-        printf("5. Salir\n");
-
-        scanf("%d", &teclado);
-        switch (teclado) {
-            case 1:
-                printf("Ingrese un número: ");
-                scanf("%f", &num1);
-                break;
-            case 2:
-                do {
-                    printf("1. Suma\n");
-                    printf("2. Resta\n");
-                    printf("3. Multiplicación\n");
-                    printf("4. División\n");
-                    printf("5. Seno\n");
-                    printf("6. Coseno\n");
-                    printf("7. Tangente\n");
-                    printf("8. Regresar\n");
-
-                    scanf("%d", &operacion);
-                    switch (operacion) {
-                        case 1:
-                            printf("Ingrese el segundo número: ");
-                            scanf("%f", &num2);
-                            resultado = suma(num1, num2);
-                            printf("La suma es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 2:
-                            printf("Ingrese el segundo número: ");
-                            scanf("%f", &num2);
-                            resultado = resta(num1, num2);
-                            printf("La resta es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 3:
-                            printf("Ingrese el segundo número: ");
-                            scanf("%f", &num2);
-                            resultado = multiplicacion(num1, num2);
-                            printf("La multiplicación es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 4:
-                            printf("Ingrese el segundo número: ");
-                            scanf("%f", &num2);
-                            resultado = division(num1, num2);
-                            printf("La división es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 5:
-                            resultado = seno(num1);
-                            printf("El seno es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 6:
-                            resultado = coseno(num1);
-                            printf("El coseno es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        case 7:
-                            resultado = tangente(num1);
-                            printf("La tangente es: %.2f\n", resultado);
-                            num1 = resultado;
-                            break;
-                        default:
-                            break;
-                    }
-                } while (operacion != 8);
-                break;
-            case 3:
-                // Limpiar Ultimo
-                num1 = 0;
-                break;
-            case 4:
-                // Limpiar todo
-                num1 = 0;
-                num2 = 0;
-                break;
-            default:
-                break;
+    case '/':
+        if (stack[0] == 0)
+        {
+            printf("\033[2J");
+            printf("Math Error\n");
+            break;
         }
-    } while (teclado != 5);
+        resultado = stack[1] / stack[0];
+        stack[0] = resultado;
+        mover();
+        break;
 
+    case 'r':
+        if (stack[0] < 0)
+        {
+            printf("\033[2J");
+            printf("Math Error\n");
+            break;
+        }
+        resultado = sqrt(stack[0]);
+        stack[0] = resultado;
+        break;
+
+    case 'p':
+        resultado = pow(stack[1], stack[0]);
+        stack[0] = resultado;
+        break;
+
+    case 's':
+        resultado = sin(stack[0]);
+        stack[0] = resultado;
+        break;
+
+    case 'c':
+        resultado = cos(stack[0]);
+        stack[0] = resultado;
+        break;
+
+    case 't':
+        if (stack[0] == 90 || stack[0] == 270)
+        {
+            printf("\033[2J");
+            printf("Math Error\n");
+            break;
+        }
+        resultado = tan(stack[0]);
+        stack[0] = resultado;
+        break;
+
+    default:
+        printf("Opción incorrecta");
+        break;
+    }
+}
+
+void cleanlast(void)
+{
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        stack[i] = stack[i + 1];
+    }
+}
+
+void cleanall(void)
+{
+    int i;
+    for (i = 0; i < 8; i++)
+    {
+        stack[i] = 0;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int opcion;
+    do
+    {
+        mostrarPila();
+        printf("------ CALCULADORA RPN ------\n\n");
+        printf("\n Ingrese el número de la opción que desa realizar \n"
+               "1. Ingresar número\n"
+               "2. Selecionar operación\n"
+               "3. Limpiar último\n"
+               "4. Limpiar todo\n"
+               "5. Salir \n");
+        scanf("%d", &opcion);
+        switch (opcion)
+        {
+        case 1:
+            printf("\033[2J");
+            mostrarPila();
+            ingresarNum();
+            printf("\033[2J");
+            break;
+
+        case 2:
+            printf("\033[2J");
+            mostrarPila();
+            selecopera();
+            // printf("\033[2J");
+            break;
+
+        case 3:
+            cleanlast();
+            printf("\033[2J");
+            break;
+
+        case 4:
+            cleanall();
+            printf("\033[2J");
+            break;
+
+        default:
+            printf("Ha finalizado exitosamente");
+            break;
+        }
+    } while (opcion < 5);
     return 0;
 }
